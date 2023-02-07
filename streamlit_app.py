4ac216989feeedc45a9631a1b3fbd88a322f953b
NigelTufnel084 <124631354+NigelTufnel084@users.noreply.github.com>
2023-02-07 14:56:24 -0500

    Update streamlit_app.py
    Extended breakfast menu and menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.title("Breakfast Menu")
+
+streamlit.title("Omega 3 & Blueberry Oatmeal")
+
+streamlit.title("Kale, Spinach & Rocket Smoothie")
+
+streamlit.title("Hard-Boiled Free-Tange Egg")
+
