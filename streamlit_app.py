74b1f5b467a5595ae08f1c9a53c2e7a36679bd9c
NigelTufnel084 <124631354+NigelTufnel084@users.noreply.github.com>
2023-02-07 15:27:05 -0500

    Update streamlit_app.py
    Fixed array name in multiselect line

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Lets put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
 
 # Display the table 
 streamlit.dataframe(my_fruit_list)
