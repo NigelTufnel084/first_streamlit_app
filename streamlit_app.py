ebaac6d3727af37f9c3bc6cbd08a77bca4251458
NigelTufnel084 <124631354+NigelTufnel084@users.noreply.github.com>
2023-02-07 15:00:06 -0500

    Update streamlit_app.py
    Change all double quotes to single quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text("Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach & Rocket Smoothie")
 
