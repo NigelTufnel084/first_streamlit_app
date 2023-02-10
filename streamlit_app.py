927332d275a315c40fbc96113aeb59234ae7d303
NigelTufnel084 <124631354+NigelTufnel084@users.noreply.github.com>
2023-02-10 16:43:35 -0500

    Update streamlit_app.py
    Variable the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice fruit Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #take the JSON version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
