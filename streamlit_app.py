1868e02c7b363be7c9b1f15f2f2dc73265946934
NigelTufnel084 <124631354+NigelTufnel084@users.noreply.github.com>
2023-02-13 18:02:41 -0500

    Update streamlit_app.py
    code fix  remove kiwi default

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
     
 #LEGACY CODE
 #streamlit.header('Fruityvice fruit Advice!')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?')
 #streamlit.write('The user entered', fruit_choice)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #take the JSON version of the response and normalize it
